185b93443d1dbed4dbb34aa05f56504327240dad
Jorge Matos <jisapjisap@gmail.com>
2025-02-23 18:57:46 +0100

    feat: add Head component for dynamic page titles; update HomePage with SEO enhancements and gradient styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ import { Separator } from "./ui/separator"
 
 const Footer = () => {
   return (
-    <div>
+    <div className="p-4 pb-0">
       <div className="container min-h-16 py-4 bg-background border border-b-0 rounded-t-xl flex flex-col gap-3 items-center lg:flex-row lg:justify-between">
         <p className="text-center text-sm">
           &copy; 2025 codewithsadee
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,56 @@
+import Head from "@/components/Head"
+import { Link } from "react-router"
+import { Button } from "@/components/ui/button"
+import { heroBannerSm, heroBannerLg } from "@/assets"
 
 
 
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+      <Head title="Tasky AI - AI-Powered To-Do List & Project Management App" />
+      <section>
+        <div className="container !px-8 grid grid-cols-1 gap-8 items-center xl:gap-12 xl:grid-cols-[1fr_1.5fr]">
+          <div className="flex flex-col items-center text-center space-y-4 lg:text-left lg:items-start lg:space-y-6">
+            <h1 className="text-4xl font-semibold max-w-[22ch] md:text-5xl lg:text-6xl">
+              Simplify Your Work and Life with{" "} 
+              <span className="inline-flex gradient-span">AI-Powered</span> 
+              {" "}Task Management
+            </h1>
+
+            <p className="max-w-[48ch] text-foreground/80 md:text-lg lg:text-xl">
+              Simplify life for both you and your team with the world&apos;s #1 task manager and to-do list app.
+            </p>
+
+            <Button asChild size="lg">
+              <Link to="register">
+                Start for free
+              </Link>
+            </Button>
+          </div>
+
+          <figure className="bg-secondary rounded-2xl overflow-hidden aspect-square max-md:max-w-[480px] max-md:mx-auto md:aspect-video">
+            <img 
+              src={heroBannerSm}
+              width={480}
+              height={480}
+              alt="Tasky AI Website"
+              className="md:hidden"
+              // por encima de 768px
+            />
+            <img
+              src={heroBannerLg}
+              width={960}
+              height={540}
+              alt="Tasky AI Website"
+              className="max-md:hidden"
+              // menor o igual a 768px
+            />
+          </figure>
+        </div>
+      </section>
+    </>
   )
 }
 
