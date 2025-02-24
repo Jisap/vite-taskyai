a3d06855f00d0691a792cf8bbe648aa4197b77c2
Jorge Matos <jisapjisap@gmail.com>
2025-02-24 20:33:48 +0100

    feat: enhance RootErrorBoundary with 404 image and additional navigation options

--- src/pages/RootErrorBoundary.tsx
+++ src/pages/RootErrorBoundary.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
 
-
+import { pageNotFound } from "@/assets"
 
 const RootErrorBoundary = () => {
 
@@ -13,27 +13,47 @@ const RootErrorBoundary = () => {
   return (
     <div className="min-h-[100dvh] flex flex-col">
       <Header />
-      <div className="grow container">
-        <h1>
+
+      <div className="grow container flex flex-col justify-center items-center pt-32">
+        <h1 className="">
           {isRouteErrorResponse(error) 
             ? "Hmmm, that page doesn't exist."
             : "Something went wrong."
           }
         </h1>
+
         <p>
           {isRouteErrorResponse(error)
             ? "You can get back on track and manage your tasks with ease."
             : "We&apos;re working on fixing this issue."
           }
         </p>
+
         <div>
           <Button asChild>
             <Link to="/">
               Return to Home
             </Link>
           </Button>
-        </div>
+
+          <Button asChild variant="ghost">
+            <Link to="/app/inbox">
+              View Inbox
+            </Link>
+          </Button>
+        </div>  
+
+        <figure className="mt-10">
+          <img 
+            src={pageNotFound}
+            width={560}
+            height={373}
+            alt="404 page not found"
+            className=""
+          />  
+        </figure>    
       </div>
+
       <Footer />
     </div>
   )
