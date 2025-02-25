7dc3d54dc4d299ad011149ba9e6f9650d6f1cb63
Jorge Matos <jisapjisap@gmail.com>
2025-02-25 19:02:49 +0100

    feat: integrate Clerk for authentication and add registration page

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import { RouterProvider } from 'react-router'
 import router from './routes/index.tsx'
+import { ClerkProvider } from '@clerk/clerk-react'
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Clerk Publishable Key")
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <RouterProvider router={router} />
+    </ClerkProvider>
   </StrictMode>,
 )
