8e4ae590423c8d7bc75cdccab23e55eb0fe4f0b7
Jorge Matos <jisapjisap@gmail.com>
2025-02-27 18:08:00 +0100

    feat: add Popover and ScrollArea components; update TaskForm to utilize new UI components; modify package dependencies

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,12 +1,42 @@
-
-
+import { Button } from "./ui/button"
+import { Calendar } from "./ui/calendar"
+import { 
+  Card,
+  CardContent,
+  CardFooter,
+} from "./ui/card"
+import { 
+  Command,
+  CommandInput,
+  CommandList,
+  CommandEmpty,
+  CommandGroup,
+  CommandItem,
+} from "./ui/command"
+import { 
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "./ui/popover"
+import { ScrollArea } from "./ui/scroll-area"
+import { Separator } from "./ui/separator"
+import { Textarea } from "./ui/textarea"
+import { 
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+} from "./ui/tooltip"
 
 
 
 
 const TaskForm = () => {
   return (
-    <div>TaskForm</div>
+    <Card className="focus-within:border-foreground/30">
+      <CardContent className="p-2">
+        <Textarea className="!border-0 !ring-0 mb-2 p-1"/>
+      </CardContent>
+    </Card>
   )
 }
 
