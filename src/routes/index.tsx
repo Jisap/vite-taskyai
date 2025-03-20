3992b1bb9fcc755ed9e0802264fd1a682eb4eac4
Jorge Matos <jisapjisap@gmail.com>
2025-03-20 18:45:10 +0100

    feat: add appLoader for fetching user projects and handling authentication

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -21,6 +21,7 @@ import ProjectsPage from "@/pages/ProjectsPage";
 import projectsLoader from "./loaders/projectsLoader";
 import ProjectDetailPage from "@/pages/ProjectDetailPage";
 import projectDetailLoader from "./loaders/projectDetailLoader";
+import appLoader from "./loaders/appLoader";
 
 
 const rootRouteChildren: RouteObject[] = [
@@ -86,7 +87,8 @@ const router = createBrowserRouter([
     path: '/app',
     element: <AppLayout />,
     children: appRouteChildren,
-    action: appAction
+    action: appAction,
+    loader: appLoader
   }
 ])
 
---
+++ src/routes/loaders/appLoader.ts
@@ -0,0 +1,46 @@
+import { databases, Query } from "@/lib/appwrite";
+import { getUserId } from "@/lib/utils";
+import { startOfToday, startOfTomorrow } from "date-fns";
+import { redirect } from "react-router";
+import type { LoaderFunction } from "react-router";
+import type { Models } from 'appwrite';
+
+const APPWRITE_DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID as string
+
+const getProjects = async () => {
+  try {
+    return await databases.listDocuments(
+      APPWRITE_DATABASE_ID,
+      "projects",
+      [
+        Query.select([
+          "$id",
+          "name",
+          "color_name",
+          "color_hex",
+          "$createdAt"
+        ]),
+        Query.orderDesc("$createdAt"),
+        Query.limit(100),
+        Query.equal("userId", getUserId()),
+      ]
+    );
+  } catch (error) {
+    console.log("Error getting projects: ", error);
+    throw new Error("Error getting projects");
+  }
+}
+
+
+const appLoader: LoaderFunction = async () => {
+  const userId = getUserId();
+
+  if (!userId) {
+    redirect("/login");
+  }
+
+  const projects = await getProjects();
+  console.log("projects", projects);
+}
+
+export default appLoader;
